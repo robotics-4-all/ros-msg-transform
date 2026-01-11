78861eb59050ca5123e23aae5b44727cc308043b
Konstantinos Panayiotou <klpanagi@gmail.com>
2026-01-11 10:25:42 +0200

    Towards versioning with bump2version

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: test build install clean
+.PHONY: test build install clean bump-major bump-minor bump-patch
 
 PYTHON = python3
 PIP = pip3
@@ -15,3 +15,12 @@ install:
 clean:
 	rm -rf dist/ build/ *.egg-info
 	find . -type d -name "__pycache__" -exec rm -rf {} +
+
+bump-major:
+	bumpversion major
+
+bump-minor:
+	bumpversion minor
+
+bump-patch:
+	bumpversion patch
--- ros_msg_transform/__init__.py
+++ ros_msg_transform/__init__.py
@@ -1,15 +1,28 @@
 from __future__ import absolute_import
 
+__version__ = "0.2.3"
+
 from .transform import (
-    dict_to_ros_msg, ros_msg_to_dict, get_message_class,
-    dict_to_ros_srv_request, dict_to_ros_srv_response,
-    ros_srv_req_to_dict, ros_srv_resp_to_dict,
-    get_service_class, fill_ros_message
+    dict_to_ros_msg,
+    dict_to_ros_srv_request,
+    dict_to_ros_srv_response,
+    fill_ros_message,
+    get_message_class,
+    get_service_class,
+    ros_msg_to_dict,
+    ros_srv_req_to_dict,
+    ros_srv_resp_to_dict,
 )
 
 __all__ = [
-    "dict_to_ros_msg", "ros_msg_to_dict", "get_message_class",
-    "dict_to_ros_srv_request", "dict_to_ros_srv_response",
-    "ros_srv_req_to_dict", "ros_srv_resp_to_dict",
-    "get_service_class", "fill_ros_message"
+    "dict_to_ros_msg",
+    "ros_msg_to_dict",
+    "get_message_class",
+    "dict_to_ros_srv_request",
+    "dict_to_ros_srv_response",
+    "ros_srv_req_to_dict",
+    "ros_srv_resp_to_dict",
+    "get_service_class",
+    "fill_ros_message",
+    "__version__",
 ]
